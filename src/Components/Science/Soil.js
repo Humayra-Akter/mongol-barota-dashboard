0d46af1fa0148d83ff8fd038532d0651a18805d7
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-02-03 12:43:37 +0600

    branch deleted

--- src/Components/Science/Soil.js
+++ src/Components/Science/Soil.js
@@ -1,7 +1,27 @@
 import React from "react";
 
 const Soil = () => {
-  return <div></div>;
+  return (
+    <div className="grid grid-cols-2">
+      <div>
+        <div>
+          <p className="text-justify p-20">
+            <p> *We used the biuret reagent for the protein test. </p>
+            <p>
+              {" "}
+              *The detection of Carbohydrate was done by using Benedict’s
+              solution and Iodine solution.
+            </p>{" "}
+            <p>
+              *The MQ135 gas sensor is used to detect the presence of ammonia or
+              its ions in the soil.
+            </p>
+          </p>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Soil;
